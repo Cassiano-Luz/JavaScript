b929bdde56a2754d114a3f5f02f72738ada5eae7
Cassiano-Luz <cassianovl@hotmail.com>
2023-09-13 17:24:06 -0300

    teste1

--- aula14/ambiente.js
+++ aula14/ambiente.js
@@ -1,3 +1,17 @@
-for(var c = 1;c <= 3;c++){
-    console.log(c)
+var c = 1
+while (c <= 8) {
+    console.log(`Esse é o laço de estrutura número ${c}`)
+    c++
 }
+
+
+
+
+
+
+
+/*console.log('Vai Começar...')
+for (var c = 1; c <= 5; c++) {
+    console.log(`Esse é o laço de número ${c}`)
+}
+console.log('FIM!')*/
