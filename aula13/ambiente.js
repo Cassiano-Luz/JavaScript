b13dace8a93b53493e6e3a28fb7e5338161088ee
Cassiano-Luz <cassianovl@hotmail.com>
2023-09-13 08:20:04 -0300

    teste de laço

---
+++ aula13/ambiente.js
@@ -0,0 +1,23 @@
+var c = 1
+ do {
+    console.log(`Passo ${c}`)
+    c++
+} while(c <= 3)
+
+
+
+// ESTRUTURA WHILE - TESTE LÓGICO NO INÍCIO, mais encontrada nos programas
+/*var c = 1
+while(c <= 5) {
+    console.log(`Passo ${c}`)
+    c++
+}*/
+
+
+
+/*console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')*/
