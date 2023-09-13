398dd76cda434b9acde80e104064a2a223654891
Cassiano-Luz <cassianovl@hotmail.com>
2023-09-13 06:08:51 -0300

    checar

--- aula12/script.js
+++ aula12/script.js
@@ -1,17 +1,16 @@
-var msg = window.document.getElementById('msg')
-var img = window.document.getElementById('img')
-var section = window.document.querySelector('section')
-var b = window.document.querySelector('body')
-
 var data = new Date()
 var ano = data.getFullYear()
-var fano = document.querySelector('txtano')
+var fano = document.getElementById('txtano')
 var res = document.querySelector('res')
 
+
 function checar() {
-    if (fano.value.lenght == 0 || fano.value > ano) {
+
+    if (fano.value.length == 0 || fano.value > ano) {
         window.alert('[ERRO] Verifique os dados e tente novamente!')
     } else {
-        window.alert('tudo ok')
+        var fsex = document.getElementById('radsex')
+        var idade = ano - Number(fano.value)
+        res.innerHTML = `Idade calculada: ${idade}`
     }
 }
