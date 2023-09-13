e2da10d8c71112bf05acbc5ac5e60bd4ef2134a3
Cassiano-Luz <cassianovl@hotmail.com>
2023-09-13 06:48:49 -0300

    ok1

--- aula12/script.js
+++ aula12/script.js
@@ -1,16 +1,49 @@
-var data = new Date()
-var ano = data.getFullYear()
-var fano = document.getElementById('txtano')
-var res = document.querySelector('res')
-
-
 function checar() {
-
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('resultado')
     if (fano.value.length == 0 || fano.value > ano) {
         window.alert('[ERRO] Verifique os dados e tente novamente!')
     } else {
-        var fsex = document.getElementById('radsex')
+        var fsex = document.getElementByName('radsex')
         var idade = ano - Number(fano.value)
-        res.innerHTML = `Idade calculada: ${idade}`
+        var gênero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            gênero = 'Homem'
+            if (idade >= 0 && idade < 10) {
+                //Criança
+                img.setAttribute('src', 'h-bb.png')
+            } else if (idade < 21) {
+                //Jovem
+                img.setAttribute('src', 'h-jovem.png')
+            }else if (idade < 50) {
+                //Adulto
+                img.setAttribute('src', 'h-adulto.png')
+            }else {
+                //Idoso
+                img.setAttribute('src', 'h-idoso.png')
+            }
+            
+    } else if (fsex[1].checked) {
+        gênero = 'Mulher'
+        if (idade >= 0 && idade < 10) {
+            //Criança
+            img.setAttribute('src', 'm-bb.png')
+        } else if (idade < 21) {
+            //Jovem
+            img.setAttribute('src', 'm-jovem.png')
+        }else if (idade < 50) {
+            //Adulto
+            img.setAttribute('src', 'm-adulta.png')
+        }else {
+            //Idoso
+            img.setAttribute('src', 'm-idosa.png')
+        }
     }
+    res.innerHTML = `Detectamos ${gênero} com idade${idade}`
+    res.appendChild(img)
+}
 }
