5fa3fa5ca412ef0ce1f80aef05ef1aea864cd7a4
Cassiano-Luz <cassianovl@hotmail.com>
2023-09-14 08:53:05 -0300

    ok2

---
+++ aula15/ambiente.js
@@ -0,0 +1,10 @@
+let num = [5, 8, 2, 9, 3]
+
+num.push(1)
+num.sort()
+console.log(num)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(`O valor do primeiro vetor é ${num[0]}`)
+
+let pos = num.indexOf(12)
+console.log(`O Valor está na posição ${pos}`)
