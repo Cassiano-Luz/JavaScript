0a740d7f7b95499dfefed84e225384f6911c18dd
Cassiano-Luz <cassianovl@hotmail.com>
2023-09-15 06:47:19 -0300

    funções

---
+++ aula16/funcao04.js
@@ -0,0 +1,27 @@
+//5! = 5 x 4 x 3 x 2 x 1 = 120
+
+let z = (n1, n2) => {
+    return n1 + n2
+}
+
+console.log(z(2,2))
+
+
+
+
+
+
+
+
+
+
+
+
+/*let f = (x) => {
+    for (let c = x; c > 1; c--) {
+        let res = c * (c - 1)
+    }
+    return res
+}
+
+console.log(f(5))*/
